ee753364a2203ff02b608b4d8c6c54971830cb24
Cyber-Adi <76634267+Cyber-Adi@users.noreply.github.com>
2021-11-06 10:45:51 -0400

    Add files via upload

--- BNRoom.js
+++ BNRoom.js
@@ -1,9 +1,27 @@
-//ADD YOUR FIREBASE LINKS HERE
+user_name=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML=  "Welcome"+user_name+"!";
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-    Room_names = childKey;
-   //Start code
-
-   //End code
-   });});}
+function addRoom(){
+    room_name=localStorage.getItem("room_name").value;
+    firebase.database().ref("/").child(room_name).update({
+          purpose: "Adding Room Name"
+    });
+    localStorage.setItem("room_name",room_name);
+    window.location="kwitter_page.html";
+}
+function getData() { firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) { childKey = childSnapshot.key;
+    Room_names=childKey;
+    console.log("Room Name - "+Room_names);
+    row="<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+    document.getElementById("output").innerHTML+=row;
+});
+});
+}
 getData();
+      function redirectToRoomName(){
+            console.log(name);
+
+      localStorage.getItem("room_name", name);
+      window.location="kwitter_page.html";
+      
+      }
